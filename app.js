e2ef089435cdbe034d3bb255699175464aa6ad78
Fangyijie Wang <fangyijie.wang@fleetmatics.com>
2018-07-03 14:34:41 +0100

    Add Event creation page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var signupRouter = require('./routes/signup');
 var eventsRouter = require('./routes/events');
+var newEventRouter = require('./routes/newEvent');
 
 var app = express();
 
@@ -28,6 +29,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/signup', signupRouter);
 app.use('/events', eventsRouter);
+app.use('/newEvent', newEventRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
